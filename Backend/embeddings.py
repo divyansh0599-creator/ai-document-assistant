559f3e9be25305fffef007813cc4639995a7e176
Divyansh <divyansh.vermalocal@gmail.com>
2026-05-14 12:09:44 +0530

    Added deploymemt changes

--- Backend/embeddings.py
+++ Backend/embeddings.py
@@ -4,7 +4,7 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 def get_embeddings():
 
     embedding_model = HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2"
+        model_name="sentence-transformers/paraphrase-MiniLM-L3-v2"
     )
 
     return embedding_model
